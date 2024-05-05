e8a97def37e2926f718ef5da6c3e9d769ced7dea
Ramanuj <ramanuj0705@gmail.com>
2024-05-05 09:18:07 +0530

    building dinesh's shattered dream

---
+++ README.md
@@ -0,0 +1,3 @@
+## PIED PIPER
+
+# It's better than Dinesh's Video Calling app
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-xl">hello</h1>
+    </>
+  );
+}
+
+export default App;
