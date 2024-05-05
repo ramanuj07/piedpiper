17712bceddcfe1e58d57a0ac1b21d41886b1e935
Ramanuj <ramanuj0705@gmail.com>
2024-05-05 11:20:19 +0530

    backend almost done

--- backend/src/managers/UserManager.ts
+++ backend/src/managers/UserManager.ts
@@ -1,6 +1,7 @@
 import { Socket } from "socket.io";
+import { RoomManager } from "./RoomManager";
 
-interface User {
+export interface User {
   socket: Socket;
   name: string;
 }
@@ -8,10 +9,12 @@ interface User {
 export class UserManager {
   private users: User[];
   private queue: string[];
+  private roomManager: RoomManager;
 
   constructor() {
     this.users = [];
     this.queue = [];
+    this.roomManager = new RoomManager();
   }
 
   addUser(name: string, socket: Socket) {
@@ -21,11 +24,38 @@ export class UserManager {
     });
     this.queue.push(socket.id);
     this.clearQueue();
+    this.initHandlers(socket);
   }
 
   removeUser(socketId: string) {
     this.users = this.users.filter((user) => user.socket.id === socketId);
+    this.queue = this.queue.filter((user) => user === socketId);
   }
 
-  clearQueue() {}
+  clearQueue() {
+    if (this.queue.length < 2) {
+      return;
+    }
+
+    const user1 = this.users.find(
+      (user) => user.socket.id === this.queue.pop()
+    );
+    const user2 = this.users.find(
+      (user) => user.socket.id === this.queue.pop()
+    );
+
+    if (!user1 || !user2) {
+      return;
+    }
+    const room = this.roomManager.createRoom(user1, user2);
+  }
+
+  initHandlers(socket: Socket) {
+    socket.on("offer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
+      this.roomManager.onOffer(sdp, roomId);
+    });
+    socket.on("answer", ({ sdp, roomId }: { sdp: string; roomId: string }) => {
+      this.roomManager.onAnswer(sdp, roomId);
+    });
+  }
 }
